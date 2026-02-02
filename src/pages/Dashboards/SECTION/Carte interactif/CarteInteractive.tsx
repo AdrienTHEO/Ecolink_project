80fbf2181ef6e190f43e2f7ed354993971f77d45
AdrienTHEO <132489148+AdrienTHEO@users.noreply.github.com>
2026-02-02 02:03:53 +0100

    responsive cart

--- src/pages/Dashboards/SECTION/Carte interactif/CarteInteractive.tsx
+++ src/pages/Dashboards/SECTION/Carte interactif/CarteInteractive.tsx
@@ -438,7 +438,7 @@ const CarteInteractive: React.FC = () => {
         {/* On ajoute 'overflow-x-auto' pour le scroll mobile et 'whitespace-nowrap' pour éviter que le texte revienne à la ligne */}
             <div className="mb-6 flex items-center gap-3 overflow-x-auto pb-2 scrollbar-hide">
             <div className="flex items-center gap-2 text-gray-700 sticky left-0 bg-white/80 backdrop-blur-sm pr-2 z-10">
-                <svg className="w-5 h-5 min-w-[20px]" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <svg className="w-5 h-5 min-w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z" />
                 </svg>
             </div>
